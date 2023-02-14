34a69067db56c10bfb50a721b463bab689820079
XenonBird <bird.xenon@gmail.com>
2023-02-14 13:11:14 +0530

    Basic routing

--- db-connection.js
+++ db-connection.js
@@ -1,17 +1,22 @@
 const mongoose = require("mongoose")
 require("dotenv").config()
 
+// =========================================
+//        CONSTANTS & CONFIGARATIONS
+// =========================================
 mongoose.set('strictQuery', false);
 
 const username = process.env.DB_USER
 const password = process.env.DB_PASS
 
-
 const url = `mongodb+srv://${username}:${password}@cluster01.rvhjwez.mongodb.net/?retryWrites=true&w=majority`
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+// =========================================
+//         CONNECT TO MONGODB
+// =========================================
+mongoose.connect(url, { dbName: "fakebook", useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log('Connected to MongoDB!');
+        console.log('🟢 Connected to MongoDB\n');
     })
     .catch((error) => {
         console.error('Error connecting to MongoDB: ', error);
--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+const express = require("express")
+const morgan = require("morgan")
+const ip = require("ip")
+
 console.clear()
 require("./db-connection")
+
+// =========================================
+//        CONSTANTS & CONFIGARATIONS
+// =========================================
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.use(morgan("common"))
+
+// =========================================
+//                  ROUTES
+// =========================================
+app.get("/api/auth/register", (req, res, next) => {
+    res.json("THis is register route")
+})
+
+// =========================================
+//                  LISTEN
+// =========================================
+app.listen(port, () => {
+    console.log(`🟢 http://${ip.address()}:${port}`)
+    console.log(`🟢 http://localhost:${port}`)
+})
