c519bc14b8ecda91dbbe22d0b057a1990764ad4b
XenonBird <bird.xenon@gmail.com>
2023-02-21 20:47:50 +0530

    minor

--- middlewares/errorHandling.js
+++ middlewares/errorHandling.js
@@ -1,15 +1,17 @@
+const notFound = (req, res, next) => {
+    res.status(404);
+    const error = new Error("Not found the page that you are looking for");
+    next(error);
+};
+
 const errorHandling = (err, req, res, next) => {
-    const statusCode = req.status(res.statusCode || 500);
+    const statusCode = res.statusCode || 500;
     const message = err.message || "Internal server error";
     const stack = err.stack;
     res.status(statusCode).json({
         message,
         stack,
     });
-};
-
-const notFound = (req, res, next) => {
-    res.status(404).json("Not found the page that you are looking for");
 };
 
 module.exports = { notFound, errorHandling };
